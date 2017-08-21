fa8b3dedc8d78c0b1db5b341a3e3aefe7b4dc66e
Syed Kazmi <syedkazmi@Syeds-MacBook-Pro.local>
2017-08-21 12:12:44 +0100

    installed mongoose and setup databse connection file & database error handling in db.js file

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,3 +1,43 @@
 /**
  * Created by syedkazmi on 21/08/2017.
  */
+const mongoose = require('mongoose');
+const dbURI = 'mongodb://syed26:plzopen26@ds013848.mlab.com:13848/tokenbasedauth'
+mongoose.connect(dbURI, { useMongoClient: true });
+
+mongoose.connection.on('connected', () => {
+    console.log(`Mongoose is connected to ${dbURI}`);
+});
+
+mongoose.connection.on('error', (err) => {
+    console.log(`Mongoose encountered an error: ${err}`);
+});
+
+mongoose.connection.on('disconnected', () => {
+    console.log(`Mongoose is disconnected`);
+});
+
+const gracefulShutdown = (msg, callback) => {
+    mongoose.connection.close( () => {
+        console.log(`Mongoose disconnected through ${msg}`);
+        callback();
+    });
+};
+// For nodemon restarts
+process.once('SIGUSR2', () => {
+    gracefulShutdown('nodemon restart', () => {
+        process.kill(process.pid, 'SIGUSR2');
+    });
+});
+// For app termination
+process.on('SIGINT', () => {
+    gracefulShutdown('app termination', () => {
+        process.exit(0);
+    });
+});
+// For Heroku app termination
+process.on('SIGTERM', () => {
+    gracefulShutdown('Heroku app shutdown', () => {
+        process.exit(0);
+    });
+});
