ed9bda24eb82a366f87f1c44a51a20966a715720
Syed Kazmi <syed.kazmi@kmandt.com>
2017-08-30 11:11:51 +0100

    improved user authentication by added middleware to check token and verify users. Also implemented protected routes

--- app.js
+++ app.js
@@ -51,4 +51,5 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+
 module.exports = app;
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -6,11 +6,7 @@ const express = require('express');
 const router = express.Router();
 const {getAllUsers: findUser} = require('../controllers/users');
 const {register: registerUser, login: loginUser} = require('../controllers/authentication');
-
-// users
-router
-    .route('/users')
-    .get(findUser);
+const jwt = require('jsonwebtoken');
 
 // registration
 router
@@ -21,5 +17,32 @@ router
 router
     .route('/login')
     .post(loginUser);
+
+// middleware for token authentication
+router.use((req, res, next)=>{
+    let token = req.body.token || req.query.token ||req.headers['x-access-token'];
+
+    if(token){
+        //verify token by checking secret & expiry date
+        jwt.verify(token, process.env.JWT_SECRET, (err, decoded)=>{
+           if(err){
+               return res.json({message: "Failed Authenticate Token"})
+           } else {
+               req.decoded = decoded;
+               next();
+           }
+        });
+    } else {
+        return res
+            .status(403)
+            .send({message: "No Token Provided."});
+    }
+});
+
+// users
+router
+    .route('/users')
+    .get(findUser);
+
 
 module.exports = router;
