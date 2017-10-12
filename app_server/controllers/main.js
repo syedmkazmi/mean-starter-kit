50af0e53d8aa5d925daee41adffa2e2d4f1c9120
Syed Kazmi <syed.kazmi@kmandt.com>
2017-10-12 13:52:58 +0100

    added angular 4, setup a single package.json file to manage both angular and node/express app, setup a deployment strategy for heroku i.e postinstall scripts

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,10 +1,11 @@
 /**
  * Created by syedkazmi on 21/08/2017.
  */
-
+const path =  require('path');
 // route controller for home page
 let getHomePage = (req, res) => {
-    res.render('index', { title: 'Mean Starter App' });
+    //res.render('index', { title: 'Mean Starter App' });
+    res.sendFile(path.join(__dirname, 'public/index.html'))
 };
 
 module.exports = {
