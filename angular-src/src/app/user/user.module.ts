3bbfbddb8760cd3fd96e30dea481d83551c37638
Syed Kazmi <syed.kazmi@kmandt.com>
2017-10-16 15:38:57 +0100

    added a user module to handle authentication, and added a login component within that users module

---
+++ angular-src/src/app/user/user.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login.component';
+import { RouterModule } from "@angular/router";
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      {path: 'login', component: LoginComponent}
+    ])
+  ],
+  declarations: [LoginComponent]
+})
+export class UserModule { }
