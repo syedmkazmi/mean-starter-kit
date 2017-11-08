b8aa6b694f47b75f56ce36f1272079a779e09bf6
Syed Kazmi <syed.kazmi@kmandt.com>
2017-11-08 12:53:04 +0000

    Added a page-not-found component and a wildcard route ti redirect to that component

--- angular-src/src/app/root/app-routing.module.ts
+++ angular-src/src/app/root/app-routing.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from "@angular/router";
-
 import { WelcomeComponent } from "../welcome/welcome.component";
+import {PageNotFoundComponent} from "./components/page-not-found.component";
 import { AuthGuard } from "../authentication/guards/auth.guard";
 
 @NgModule({
   imports: [
     RouterModule.forRoot([
       { path: 'welcome', component: WelcomeComponent, canActivate: [AuthGuard]},
-      { path: '', redirectTo: 'welcome', pathMatch: 'full'}
+      { path: '', redirectTo: 'welcome', pathMatch: 'full'},
+      { path: '**', component: PageNotFoundComponent}
     ],{useHash: true})
   ],
   exports: [RouterModule]
